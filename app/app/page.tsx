a2aba75f782ba21f5f5a0c6c1b358b7360e30aa6
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-11 19:00:39 +0100

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-10 text-center">
-      <h1 className="text-4xl font-bold text-blue-600">Welcome to TemiLade Connect</h1>
-      <p className="mt-4 text-gray-600">Buy Airtime, Data, Cable TV & Electricity instantly and cheaply</p>
-      <a href="/dashboard" className="mt-6 inline-block bg-orange-500 text-white px-6 py-3 rounded-lg">Start Buying</a>
-    </main>
-  )
-}
