23b7ab0072f82316163852621980cb0cd69fa0d0
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-13 03:37:28 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/prisma'
 
 export default async function Home() {
   let dbStatus = "Connected ✅"
