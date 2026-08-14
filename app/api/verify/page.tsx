ddd92028128f7162bbfe6f299b3ce49f474399a6
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-14 18:01:09 +0100

    Create page.tsx

---
+++ app/api/verify/page.tsx
@@ -0,0 +1,8 @@
+export default function VerifyPage() {
+  return (
+    <div style={{padding: 20, textAlign: 'center'}}>
+      <h1>Enter OTP</h1>
+      <p>OTP Sent! Check the alert: 123456</p>
+    </div>
+  )
+}
