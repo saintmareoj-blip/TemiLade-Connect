2a2258d52791dd01a2012ce9b5eb76d1abde5b95
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-15 17:08:55 +0100

    Delete app directory

--- app/api/verify/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function VerifyPage() {
-  return (
-    <div style={{padding: 20, textAlign: 'center'}}>
-      <h1>Enter OTP</h1>
-      <p>OTP Sent! Check the alert: 123456</p>
-    </div>
-  )
-}
