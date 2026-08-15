362bee62fc18273bfbf8b81811aec378d0a37bea
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-15 17:38:37 +0100

    Add register page

---
+++ app/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div style={{padding: 40, textAlign: 'center'}}>
+      <h1>Create Account</h1>
+      <p>Register form will go here</p>
+    </div>
+  )
+}
