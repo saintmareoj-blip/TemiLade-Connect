41a82a42c00a891c9ee978dbea5cef630c3db3c5
saintmareoj-blip <saintmareo.j@gmail.com>
2026-08-15 17:35:27 +0100

    Create app folder and home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{padding: 40, textAlign: 'center'}}>
+      <h1>TemiLade Connect</h1>
+      <p>Home is working ✅</p>
+    </div>
+  )
+}
